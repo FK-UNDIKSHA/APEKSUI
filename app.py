05a267d8805a7782e4263d76e5e253758aa00510
Semara Incorporated <semaraincorporated@gmail.com>
2021-11-07 13:04:41 +0800

    Add Patch to Support Raspberry Pi 3

--- app.py
+++ app.py
@@ -8,52 +8,36 @@ from flask import Flask, redirect, request, jsonify, make_response, render_templ
 from miscale2 import miscale2
 from PedoScore import ZScore
 from Pedokom import pedokom
-from threading import Thread
+from threading import Thread, Event
+from queue import Queue
+from multiprocessing import Process, Queue
 
-app = Flask(__name__)
+import run
+
+app = run.main()
 
 busyo = 0
+q = None
+worker = None
+
+
 try:
     Coms = pedokom.PedoKom('/dev/ttyUSB0', 9600, .1)
 except:
     Coms = pedokom.PedoKom('/dev/ttyUSB1', 9600, .1)
 
-@app.route('/', methods=["GET"])
-def index():
-    #return render_template('index.html')
-    return render_template('index.html')
-
-def PingMe():
-    #time.sleep(2)
-    asu = Coms.SendCommand("yuhu", 1)
-    if 'uth3re?' in asu["Response"]:
-        Coms.SendCommand("metoo", 0)
-    print(asu)
-
-@app.route('/clear', methods=["GET", "POST"])
-def bersih():
+def ThreadD(q):
     global busyo
-    Coms.SendCommand("pesan1;Go WebWifi")
-    Coms.SendCommand("pesan2;=>apeks.net<=")
-
-    time.sleep(3)
-    Coms.SendCommand("pesan1;    Welcome")
-    Coms.SendCommand("pesan2;Waiting Input...")
-    busyo = 0
-    return "ok"
-
-@app.route('/measure', methods=["GET", "POST"])
-def measure():
-    global busyo
-    if busyo == 1:
-        time.sleep(3)
-
+    #if busyo == 1:
+    #    time.sleep(3)
+    
     tb = []
-    PingMe()
+    #PingMe()
     busyo =1
     #Coms = pedokom.PedoKom('/dev/ttyUSB0', 9600, .1)
     #jangan lupa IMT
-    data = request.get_json(force=True)
+    
+    data = q.get()
     print(data)
     print("MULAI JALAN")
     #Coms.SendCommand("pesan1;Ukur Berat Badan", 0)
@@ -89,11 +73,19 @@ def measure():
     #time.sleep(3)
 
     print("JSON: ", data)
-    usia = int(data.get("usia",0))
-    jk = str(data.get('kelamin', "no")).lower()
+    
+    try:
+        usia = int(data.get("usia",0))
+    except:
+        usia = 0
+    
+    try:
+        jk = str(data.get('kelamin', "no")).lower()
+    except:
+        jk = "no"
 
     Gender = ""
-    if jk == "Man":
+    if jk == "boy":
         Gender = "M"
     else:
         Gender = "F"
@@ -101,24 +93,76 @@ def measure():
     zee = ""
     if usia == 0 and jk == "no":
         zimt = ZScore.IMT(Berat, Tinggi/100)
-        zee = "IMT"
+        zee = "BMI (Body Mass Index)"
     else:
         zimt = ZScore.ZScore(usia, Berat, Gender)
         zee = "Z-Score"
 
     resp = {"zimt": zimt, "tinggi": str(Tinggi)+" CM", "berat": str(Berat), "Pesan": zee}
-
+    
     Coms.SendCommand("pesanx;Tinggi: " + str(Tinggi) + "CM?" + "Berat: " + str(Berat) + "KG", 0)
+    q.put_nowait(resp)
+    #q.task_done()
 
     #Coms.SendCommand("pesanx;Go WebWifi?=>apeks.net<=")
 
+@app.route('/', methods=["GET"])
+def index():
+    #return render_template('index.html')
+    return render_template('index.html')
+
+def PingMe():
+    #time.sleep(2)
+    asu = Coms.SendCommand("yuhu", 1)
+    if 'uth3re?' in asu["Response"]:
+        Coms.SendCommand("metoo", 0)
+    print(asu)
+
+@app.route('/sane', methods=["GET", "POST"])
+def Sane():
+    global busyo
+    print("BUSY STATE = ", busyo)
+    if busyo == 1:
+        return "ok"
+    else:
+        return "ok"
+
+@app.route('/clear', methods=["GET", "POST"])
+def bersih():
+    global busyo
+    Coms.SendCommand("pesan1;Go WebWifi")
+    Coms.SendCommand("pesan2;=>apeks.net<=")
+
+    time.sleep(3)
+    Coms.SendCommand("pesan1;    Welcome")
+    Coms.SendCommand("pesan2;Waiting Input...")
+    busyo = 0
+    return "ok"
+
+@app.route('/measure', methods=["GET", "POST"])
+def measure():
+    global q, worker, busyo
+    
+    if busyo == 1:
+        print("[DEBUG] Busy State Terminate...")
+        worker.terminate()
+
+    busyo = 1
+    q = Queue(maxsize=0)
+    data = request.get_json(force=True)
+    
+    worker = Process(target=ThreadD, args=(q,))
+    worker.daemon = True
+    worker.start()
+    
+    q.put(data)
+    
+    worker.join()
+    
+    resp = q.get()
+    busyo = 0
     return make_response(jsonify(resp), 200)
 
 if __name__ == '__main__':
-    #Start Miscale2 as Thread
-    print('Starting Miscale Watchdog...')
-    PingMe()
-    #a = Thread(target=miscale2.mainS, daemon=True)
-    #a.start()
-    print('[OK] Started Miscale Watchdog')
+    
     app.run(host='0.0.0.0', port=5050, debug=True)
---
+++ run.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from Pedokom import pedokom
+
+try:
+    Coms = pedokom.PedoKom('/dev/ttyUSB0', 9600, .1)
+except:
+    Coms = pedokom.PedoKom('/dev/ttyUSB1', 9600, .1)
+
+def PingMe():
+    #time.sleep(2)
+    asu = Coms.SendCommand("yuhu", 1)
+    if 'uth3re?' in asu["Response"]:
+        Coms.SendCommand("metoo", 0)
+    print(asu)
+
+def main():
+    app_ = Flask(__name__)
+    #Start Ping and Send Challenge Request to Arduino
+    print('Sending Challenge Request...')
+    PingMe()
+    #a = Thread(target=miscale2.mainS, daemon=True)
+    #a.start()
+    print('[OK] Challenge Received')
+    return app_
